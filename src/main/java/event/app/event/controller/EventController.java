e7d77c4f3ef12a06b823fc456b7310cf10fcda5e
GhenadieCebanu <cebanu.ghenadie@gmail.com>
2020-03-17 21:23:07 +0200

    add log

--- src/main/java/event/app/event/controller/EventController.java
+++ src/main/java/event/app/event/controller/EventController.java
@@ -3,6 +3,7 @@ package event.app.event.controller;
 import event.app.event.Event;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/get-out/events")
+@Slf4j
 public class EventController {
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Event> getAllEvents() {
+    log.info("EM: Get events");
     List<Event> events = new ArrayList<>();
     events.add(Event.builder().id(1L).title("''Aventuri cu dl. Goe'' | Ateneul National din Iasi").build());
     events.add(Event.builder().id(2L).title("Luna Francofoniei la Iasi").build());
